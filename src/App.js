d43fbd977e300a0a7dd403e0c67ba2af03f059fa
vimtor <victor@vimtor.io>
2021-12-15 21:32:09 +0100

    Add focus and scroll behaviour

--- src/App.js
+++ src/App.js
@@ -1,34 +1,46 @@
-import { useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import "./App.css";
 
 function App() {
   const [title, setTitle] = useState("");
   const [tasks, setTasks] = useState([]);
+  const inputRef = useRef();
+  const listRef = useRef();
 
   const pendingTasks = tasks.length;
+
+  useEffect(() => {
+    document.title = pendingTasks + " tasks remaining";
+  }, [pendingTasks]);
+
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
 
   return (
     <div>
       <h1>Task Manager</h1>
       <p>Pending tasks: {pendingTasks}</p>
-      <div>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          setTasks([title, ...tasks]);
+          setTitle("");
+          inputRef.current.focus();
+          listRef.current.scrollTop = 0;
+        }}
+      >
         <input
+          ref={inputRef}
           type="text"
           value={title}
           onChange={(event) => {
             setTitle(event.target.value);
           }}
         />
-        <button
-          onClick={() => {
-            setTasks([title, ...tasks]);
-            setTitle("");
-          }}
-        >
-          Add
-        </button>
-      </div>
-      <ul className="task-list">
+        <button type="submit">Add</button>
+      </form>
+      <ul ref={listRef} className="task-list">
         {tasks.map((task) => {
           return (
             <li
